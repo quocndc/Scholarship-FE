15d9a504cf57732952091f7f4fc2b10f2bdcc6a0
quocndc <anhquockkzz@gmail.com>
2024-11-02 13:23:14 +0700

    otp, tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,3 +38,19 @@ export default {
   },
   plugins: [animations, components, rounded, shade, visualizations, tailwindAnimations, typography],
 };
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
+      },
+    },
+  },
+}
