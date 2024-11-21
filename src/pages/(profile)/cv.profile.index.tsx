9be8d7edd0d0ea4d0d6a91236083d02763f9a2b6
gftantan <thenight@gmail.com>
2024-11-21 21:45:59 +0700

    (profile)

--- src/pages/(profile)/cv.profile.index.tsx
+++ src/pages/(profile)/cv.profile.index.tsx
@@ -4,16 +4,21 @@ import DataTable from '@components/data-table/DataTable';
 import TopBar from '@components/data-table/Topbar';
 import ResumeDetailPanel from '@components/resume-details/ResumeDetailPanel';
 import StatusBadge from '@components/resume-details/StatusBadge';
+import { UpdateResumePanel, UpdateResumeSchema } from '@components/resume-details/UpdateResumePanel';
+import { useUpdateResumeStatus } from '@components/resume-details/useUpdateResumeStatus';
+import Button from '@components/tailus-ui/Button';
 import { Caption, Text } from '@components/tailus-ui/typography';
 import { Resume, SchoolarShip } from '@lib/types';
-import { IconEye } from '@tabler/icons-react';
+import { IconDownload, IconEdit, IconEye } from '@tabler/icons-react';
 import { ColumnDef } from '@tanstack/react-table';
 import { useMemo, useState } from 'react';
+import { toast } from 'sonner';
 
 function CvProfile() {
   const { isLoading, data } = useGetUserCv();
   const [selectedItems, setSelectedItems] = useState<Resume[]>();
   const [isDetailPanelOpen, setIsDetailPanelOpen] = useState(false);
+  const [isUpdateStatusPanelOpen, setIsUpdateStatusPanelOpen] = useState(false);
   const actions = useMemo<TopbarAction[][]>(
     () => [
       selectedItems?.length === 1
@@ -24,6 +29,14 @@ function CvProfile() {
               size: 'sm',
               variant: 'soft',
               onClick: () => setIsDetailPanelOpen(true),
+            },
+            {
+              label: 'Sửa',
+              icon: <IconEdit />,
+              size: 'sm',
+              intent: 'secondary',
+              variant: 'soft',
+              onClick: () => setIsUpdateStatusPanelOpen(true),
             },
           ]
         : [],
@@ -63,6 +76,21 @@ function CvProfile() {
         },
       },
       {
+        accessorKey: 'urlCV',
+        header: 'CV',
+        cell: (row) => {
+          const cv = row.getValue() as string;
+          return (
+            <Button.Root size={'sm'} variant="ghost" href={cv} target="_blank" rel="noreferrer">
+              <Button.Icon>
+                <IconDownload />
+              </Button.Icon>
+              <Button.Label>Download</Button.Label>
+            </Button.Root>
+          );
+        },
+      },
+      {
         accessorKey: 'createdAt',
         header: 'Ngày tạo',
         cell: (row) => <Caption>{new Date(row.getValue() as string).toLocaleDateString()}</Caption>,
@@ -75,9 +103,22 @@ function CvProfile() {
     const filtered = item.filter((item) => ids.includes(item._id)) ?? [];
     setSelectedItems(filtered);
   };
+  const { mutateAsync } = useUpdateResumeStatus();
+
+  const updateStatus = async (data: UpdateResumeSchema) => {
+    toast.promise(mutateAsync(data), {
+      loading: 'Đang cập nhật ...',
+      success: () => {
+        setIsUpdateStatusPanelOpen(false);
+        return 'Cập nhật thành công';
+      },
+      error: 'Cập nhật thất bại',
+    });
+  };
   return (
     <div>
       <ResumeDetailPanel open={isDetailPanelOpen} onOpenChange={setIsDetailPanelOpen} item={selectedItems?.[0]} />
+      <UpdateResumePanel open={isUpdateStatusPanelOpen} onOpenChange={setIsUpdateStatusPanelOpen} item={selectedItems?.[0]} onSubmit={updateStatus} />
       <div className="min-h-12">
         <TopBar selectedItems={selectedItems} actions={actions} totalItems={data?.data.length} />
       </div>
