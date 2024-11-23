2f9b01617606b2daa98224eda58587bf757bcc3e
gftantan <thenight@gmail.com>
2024-11-23 23:50:41 +0700

    schoolar-list

---
+++ src/components/schoolar-list/useCreateScholarShip.ts
@@ -0,0 +1,28 @@
+import { getScholarShipKey } from '@components/schoolar-list/constant';
+import { CreateScholarSchema } from '@components/schoolar-list/CreateScholarPanel';
+import { useUploadBatchImages } from '@components/upload/useUploadBatchImages';
+import axios from '@lib/axios';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+export function useCreateScholarShip() {
+  const client = useQueryClient();
+  const { mutateAsync } = useUploadBatchImages();
+  return useMutation({
+    mutationFn: async (data: CreateScholarSchema) => {
+      const imagesToUpload = data.image.filter((i) => i instanceof File);
+      const images = data.image.filter((i) => !(i instanceof File));
+      if (imagesToUpload.length > 0) {
+        const res = await mutateAsync(imagesToUpload);
+        images.push(...res.map((r) => r.url));
+      }
+
+      return axios.post('/scholarship', {
+        ...data,
+        image: images,
+      });
+    },
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: getScholarShipKey.list() });
+    },
+  });
+}
